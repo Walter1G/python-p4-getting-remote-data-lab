b1574aa73fea59a0770be91e1ff0123aa2d81556
Walter N <2760walter@gmail.com>
2024-01-12 17:38:53 +0300

    Complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -5,9 +5,15 @@ class GetRequester:
 
     def __init__(self, url):
         self.url = url
-
+        self.get_response_body()
+        
+    
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return json.loads(response_body)
+      
+        
